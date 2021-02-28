4aabe6aa6837c3b7026c6448ff85ccc1363cf785
andraderegis <reginaldo.andrade@outlook.com>
2021-02-28 18:25:17 -0300

    Finish ChallengeBox component stylished

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,15 +1,43 @@
 import styles from 'styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
+  const hasActiveChallenge = true;
+
   return (
     <div className={styles.challengeBoxContainer}>
-      <div className={styles.challengeNotActive}>
-        <strong>Finalize um ciclo para receber um desafio</strong>
-        <p>
-          <img src="icons/level-up.svg" alt="Level Up" />
-          Avance de level completando desafios.
-        </p>
-      </div>
+      { hasActiveChallenge
+        ? (
+          <div className={styles.challengeActive}>
+            <header>Ganha 400 xp</header>
+            <main>
+              <img src="icons/body.svg" alt="Challenge" />
+              <strong>Novo Desafio</strong>
+              <p>Levante e não deixe de fazer alongamentos. É importante para as articulações.</p>
+            </main>
+
+            <footer>
+              <button
+                type="button"
+                className={styles.challengeFailedButton}>
+                Falhei
+                </button>
+              <button type="button"
+                className={styles.challengeSucceedButton}>
+                Completei
+                </button>
+            </footer>
+          </div>
+        )
+        : (
+          <div className={styles.challengeNotActive}>
+            <strong>Finalize um ciclo para receber um desafio</strong>
+            <p>
+              <img src="icons/level-up.svg" alt="Level Up" />
+              Avance de level completando desafios.
+            </p>
+          </div>
+        )
+      }
     </div>
   );
 }
